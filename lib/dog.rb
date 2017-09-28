daa6c216f8b87367b9dca3360ae639d7de582730
John Tuttle <thejwt@gmail.com>
2017-09-28 18:41:37 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,19 @@
 class Dog
   @@all = []
 
-  
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @@all << name
+  end
+
+  def self.all
+    puts @@all
+  end
+
+  def self.clear_all
+    @@all = []
+  end
+
 end
