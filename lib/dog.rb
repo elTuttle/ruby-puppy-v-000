b6afa75acea4ecae588bb19f9042945b9431906a
John Tuttle <thejwt@gmail.com>
2017-09-28 18:54:23 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,26 @@
 class Dog
   @@all = []
+  @@all_with_id = []
 
   attr_accessor :name
 
   def initialize(name)
-    @name = name
-    @@all << name
+    @name = name    
   end
 
   def self.all
+    @@all.each
     puts @@all
   end
 
   def self.clear_all
     @@all = []
+  end
+
+  private
+
+  def adding_name
+    
   end
 
 end
